9a56fea0ef027295049a96fe8295a7fe12f31e3f
lbyrapuneni <lbyrapuneni@gmail.com>
2024-06-29 12:32:53 -0700

    writing function acces board

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -10,16 +10,36 @@ class Board:
             for x in range(3):
                 j.append(["_", "_", "_"])
 
-    def prettyprint(self):
+    def __str__(self):
         for m in self.board:
             print()
             for x in m:
                 print(x, sep=" ", end=" ")
                 print()
 
-    def printcell(self, number):
-            cell = self.board[number-1]
-            for k in cell:
-                print(k)
-                print()
-
+    def print_cell(self, number):
+        cell = self.board[number-1]
+        for k in cell:
+            print(k)
+            print()
+    def add_num(self, num, row, column):
+        if row <= 3:
+            currentrow = [self.board[0][row-1], self.board[1][row-1], self.board[2][row-1]]
+            if column <=3:
+                currentrow[0][column-1] = num
+            elif column <=6:
+                currentrow[1][column-4] = num
+            elif column <=9:
+                currentrow[2][column-7] = num
+        elif row <=6:
+            currentrow = [self.board[0][row - 1], self.board[1][row - 1], self.board[2][row - 1]]
+            if column <= 3:
+                currentrow[0][column - 1] = num
+            elif column <= 6:
+                currentrow[1][column - 4] = num
+            elif column <= 9:
+                currentrow[2][column - 7] = num
+test = Board()
+test.empty_board()
+test.add_num(2,2,2)
+print(test)
